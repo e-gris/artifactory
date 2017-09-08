9d72f1cd99c8a5bfc9902b438301fa5194c89c6c
Eric Griswold <eric.griswold@puppet.com>
2017-09-07 17:24:40 -0700

    remove java stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,6 @@
 #
 #
 class artifactory(
-  Boolean $manage_java                                                    = false,
   String $yum_name                                                        = 'bintray-jfrog-artifactory-rpms',
   String $yum_baseurl                                                     = 'http://jfrog.bintray.com/artifactory-rpms',
   String $package_name                                                    = 'jfrog-artifactory-oss',
@@ -19,35 +18,11 @@ class artifactory(
   Optional[String] $binary_provider_filesystem_dir                        = undef,
   Optional[String] $binary_provider_cache_dir                             = undef,
 ) {
-  $artifactory_home = '/var/opt/jfrog/artifactory'
-
+  $artifactory_home = '/opt/jfrog/artifactory'
   $service_name = 'artifactory'
 
-  if($manage_java) {
-    # Ensure other open-jdk packages are removed
-    $remove_jdks = [
-      'java-1.6.0-openjdk-devel',
-      'java-1.6.0-openjdk',
-      'java-1.7.0-openjdk-devel',
-      'java-1.7.0-openjdk',
-    ]
-
-    package { $remove_jdks:
-      ensure => absent,
-    }
-
-    class{'::java':
-      version => latest,
-      package => 'java-1.8.0-openjdk-devel',
-    }
-  }
-
-  Class['::java']
-  -> class{'::artifactory::yum': }
+  class{'::artifactory::yum': }
   -> class{'::artifactory::install': }
   -> class{'::artifactory::config': }
   ~> class{'::artifactory::service': }
-
-  # Make sure java is included
-  include ::java
 }
