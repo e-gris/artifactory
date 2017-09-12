48ebb1293d6b3356eeea0b60faa032107358486a
Eric Griswold <eric.griswold@puppet.com>
2017-09-12 11:19:14 -0700

    Start working on the database variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -38,62 +38,127 @@ class artifactory::config {
     owner  => 'artifactory',
     group  => 'artifactory',
   }
-  
-  # Install storage.properties if Available
-  if(
-    $::artifactory::jdbc_driver_url or
-    $::artifactory::db_url or
-    $::artifactory::db_username or
-    $::artifactory::db_password or
-    $::artifactory::db_type) {
-    if ($::artifactory::jdbc_driver_url and
-        $::artifactory::db_url and
-        $::artifactory::db_username and
-        $::artifactory::db_password and
-        $::artifactory::db_type
-        ) {
-      file { "${::artifactory::artifactory_home}/etc/db.properties":
-        ensure  => file,
-        content => epp(
-          'artifactory/db.properties.epp',
-          {
-            db_url                         => $::artifactory::db_url,
-            db_username                    => $::artifactory::db_username,
-            db_password                    => $::artifactory::db_password,
-            db_type                        => $::artifactory::db_type,
-            binary_provider_type           => $::artifactory::binary_provider_type,
-            pool_max_active                => $::artifactory::pool_max_active,
-            pool_max_idle                  => $::artifactory::pool_max_idle,
-            binary_provider_cache_maxsize  => $::artifactory::binary_provider_cache_maxsize,
-            binary_provider_filesystem_dir => $::artifactory::binary_provider_filesystem_dir,
-            binary_provider_cache_dir      => $::artifactory::binary_provider_cache_dir,
-          }
-        ),
-        mode    => '0664',
-      }
 
-      file { "${::artifactory::artifactory_home}/etc/storage.properties":
-        ensure => link,
-        target => "${::artifactory::artifactory_home}/etc/db.properties",
-      }
+  ## Create binarystore.xml
 
-      $file_name =  regsubst($::artifactory::jdbc_driver_url, '.+\/([^\/]+)$', '\1')
+  ## Create db.properties
+  # Install db.properties if we can
+  $database_variables = [
+    $::artifactory::jdbc_driver_url,
+    $::artifactory::db_url,
+    $::artifactory::db_username,
+    $::artifactory::db_password,
+    $::artifactory::db_type]
+  $database_variables_size = size($database_variables)
 
+  ##
+  ## Try to figure out of some, all or none of the database_variables
+  ## have been set.
+  ##
+  $database_variables_defined = $database_variables.filter | $dbv | {
+    defined($dbv)
+  }
+  $database_variables_defined_size = size($database_variables_defined)
 
-      file { "${::artifactory::artifactory_home}/tomcat/lib":
-        ensure => directory,
-        mode   => '755',
-        owner  => 'artifactory',
-      }
-      
-      file { "${::artifactory::artifactory_home}/tomcat/lib/${file_name}":
-        source => $::artifactory::jdbc_driver_url,
-        mode   => '0644',
-        owner  => 'artifactory',
-      }
+  if ($database_variables_defined_size == 0) {
+    info("No database details provided, providing default")
+  }
+  elsif ($database_variables_defined_size != $database_variables_size) {
+    warning('Database port, hostname, username, password and type must be all be set, or not set. Install proceeding without storage.')
+  }
+  else {
+    file { "${::artifactory::artifactory_home}/etc/db.properties":
+      ensure  => file,
+      content => epp(
+        'artifactory/db.properties.epp', {
+          db_url                         => $::artifactory::db_url,
+          db_username                    => $::artifactory::db_username,
+          db_password                    => $::artifactory::db_password,
+          db_type                        => $::artifactory::db_type,
+          binary_provider_type           => $::artifactory::binary_provider_type,
+          pool_max_active                => $::artifactory::pool_max_active,
+          pool_max_idle                  => $::artifactory::pool_max_idle,
+          binary_provider_cache_maxsize  => $::artifactory::binary_provider_cache_maxsize,
+          binary_provider_filesystem_dir => $::artifactory::binary_provider_filesystem_dir,
+          binary_provider_cache_dir      => $::artifactory::binary_provider_cache_dir,
+        }),
+      mode    => '0664',
     }
-    else {
-      warning('Database port, hostname, username, password and type must be all be set, or not set. Install proceeding without storage.')
+
+    file { "${::artifactory::artifactory_home}/etc/storage.properties":
+      ensure => link,
+      target => "${::artifactory::artifactory_home}/etc/db.properties",
+    }
+
+    $file_name =  regsubst($::artifactory::jdbc_driver_url, '.+\/([^\/]+)$', '\1')
+
+    file { "${::artifactory::artifactory_home}/tomcat/lib":
+      ensure => directory,
+      mode   => '755',
+      owner  => 'artifactory',
+    }
+      
+    file { "${::artifactory::artifactory_home}/tomcat/lib/${file_name}":
+      source => $::artifactory::jdbc_driver_url,
+      mode   => '0644',
+      owner  => 'artifactory',
     }
   }
+    
+  # if(
+  #   $::artifactory::jdbc_driver_url or
+  #   $::artifactory::db_url or
+  #   $::artifactory::db_username or
+  #   $::artifactory::db_password or
+  #   $::artifactory::db_type) {
+  #   if ($::artifactory::jdbc_driver_url and
+  #       $::artifactory::db_url and
+  #       $::artifactory::db_username and
+  #       $::artifactory::db_password and
+  #       $::artifactory::db_type
+  #       ) {
+  #     file { "${::artifactory::artifactory_home}/etc/db.properties":
+  #       ensure  => file,
+  #       content => epp(
+  #         'artifactory/db.properties.epp',
+  #         {
+  #           db_url                         => $::artifactory::db_url,
+  #           db_username                    => $::artifactory::db_username,
+  #           db_password                    => $::artifactory::db_password,
+  #           db_type                        => $::artifactory::db_type,
+  #           binary_provider_type           => $::artifactory::binary_provider_type,
+  #           pool_max_active                => $::artifactory::pool_max_active,
+  #           pool_max_idle                  => $::artifactory::pool_max_idle,
+  #           binary_provider_cache_maxsize  => $::artifactory::binary_provider_cache_maxsize,
+  #           binary_provider_filesystem_dir => $::artifactory::binary_provider_filesystem_dir,
+  #           binary_provider_cache_dir      => $::artifactory::binary_provider_cache_dir,
+  #         }
+  #       ),
+  #       mode    => '0664',
+  #     }
+
+  #     file { "${::artifactory::artifactory_home}/etc/storage.properties":
+  #       ensure => link,
+  #       target => "${::artifactory::artifactory_home}/etc/db.properties",
+  #     }
+
+  #     $file_name =  regsubst($::artifactory::jdbc_driver_url, '.+\/([^\/]+)$', '\1')
+
+
+  #     file { "${::artifactory::artifactory_home}/tomcat/lib":
+  #       ensure => directory,
+  #       mode   => '755',
+  #       owner  => 'artifactory',
+  #     }
+      
+  #     file { "${::artifactory::artifactory_home}/tomcat/lib/${file_name}":
+  #       source => $::artifactory::jdbc_driver_url,
+  #       mode   => '0644',
+  #       owner  => 'artifactory',
+  #     }
+  #   }
+  #   else {
+  #     warning('Database port, hostname, username, password and type must be all be set, or not set. Install proceeding without storage.')
+  #   }
+  # }
 }
