f90493ca44c29c0ec165367a65dfa3aef2acf5f9
Eric Griswold <eric.griswold@puppet.com>
2017-09-19 15:50:03 -0700

    manage system properties

--- manifests/config.pp
+++ manifests/config.pp
@@ -28,6 +28,13 @@ class artifactory::config {
     group  => 'artifactory',
   }
 
+  file { "/etc/opt/jfrog/artifactory/artifactory.system.properties":
+    ensure => present,
+    owner  => 'artifactory',
+    group  => 'artifactory',
+    mode   => '0644',
+  }
+
   file { "/var/opt/jfrog/run":
     ensure => directory,
     owner  => 'artifactory',
@@ -52,41 +59,28 @@ class artifactory::config {
   }
   $database_variables_defined_size = size($database_variables_defined)
 
-  # if (!$is_primary) {
-  #   notify { "HA secondary node. No db.properties needed": }
-  #   info("HA secondary node. No db.properties needed")
-  # }
-  # elsif ($database_variables_defined_size == 0) {
-  #   notify { "Default database": }
-  #   info("No database details provided, providing default")
-  # }
-  # elsif ($database_variables_defined_size != $database_variables_size) {
-  #   warning('Database port, hostname, username, password and type must be all be set, or not set. Install proceeding without storage.')
-  # }
-  # else {
-  #   file { "${::artifactory::artifactory_home}/etc/db.properties":
-  #     ensure  => file,
-  #     content => epp(
-  #       'artifactory/db.properties.epp', {
-  #         db_url                         => $::artifactory::db_url,
-  #         db_username                    => $::artifactory::db_username,
-  #         db_password                    => $::artifactory::db_password,
-  #         db_type                        => $::artifactory::db_type,
-  #       }),
-  #     mode    => '0664',
-  #   }
-
-  #   # $file_name =  regsubst($::artifactory::jdbc_driver_url, '.+\/([^\/]+)$', '\1')
-  #   # file { "${::artifactory::artifactory_home}/tomcat/lib":
-  #   #   ensure => directory,
-  #   #   mode   => '755',
-  #   #   owner  => 'artifactory',
-  #   # }
-      
-  #   # file { "${::artifactory::artifactory_home}/tomcat/lib/${file_name}":
-  #   #   source => $::artifactory::jdbc_driver_url,
-  #   #   mode   => '0644',
-  #   #   owner  => 'artifactory',
-  #   # }
-  # }
+  if (!$is_primary) {
+    notify { "HA secondary node. No db.properties needed": }
+    info("HA secondary node. No db.properties needed")
+  }
+  elsif ($database_variables_defined_size == 0) {
+    notify { "Default database": }
+    info("No database details provided, providing default")
+  }
+  elsif ($database_variables_defined_size != $database_variables_size) {
+    warning('Database port, hostname, username, password and type must be all be set, or not set. Install proceeding without storage.')
+  }
+  else {
+    file { "${::artifactory::artifactory_home}/etc/db.properties":
+      ensure  => file,
+      content => epp(
+        'artifactory/db.properties.epp', {
+          db_url                         => $::artifactory::db_url,
+          db_username                    => $::artifactory::db_username,
+          db_password                    => $::artifactory::db_password,
+          db_type                        => $::artifactory::db_type,
+        }),
+      mode    => '0664',
+    }
+  }
 }
