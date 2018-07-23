36499db206d0d7592ef3169b519a667a209dbb95
Eric Griswold <eric.griswold@puppet.com>
2018-07-23 15:01:45 -0700

    lint service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,11 @@
+# Manage the artifactory service
 class artifactory::service {
   ## Can't manage service run/stopped state because of artifactory bug
   ## if this is a non-primary node.
 
-  if ($is_primary) {
+  if ($artifactory::is_primary) {
     service { 'artifactory':
       ensure => running,
     }
   }
 }
-    
