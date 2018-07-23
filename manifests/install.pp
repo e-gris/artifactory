04f8b10b986ee758249422b4c006a94a5e7a0aef
Eric Griswold <eric.griswold@puppet.com>
2018-07-23 15:00:39 -0700

    lint install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,15 @@
+# Pick an artifactory package to install
 class artifactory::install {
-  if ($::artifactory::package_version) {
-    $artifactory_package_ensure = $::artifactory::package_version
+  if ($artifactory::package_version) {
+    $artifactory_package_ensure = $artifactory::package_version
   }
   else {
-    $artifactory_package_ensure = "present"
+    $artifactory_package_ensure = 'present'
   }
 
-  info("Artifactory version set to \"$artifactory_package_ensure\"")
-  
-  package { $::artifactory::package_name:
+  info("Artifactory version set to \"${artifactory_package_ensure}\"")
+
+  package { $artifactory::package_name:
     ensure  => $artifactory_package_ensure,
   }
 }
